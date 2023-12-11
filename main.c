302ab33c0e3cf5d4df666e7c875de84132549560
RaylannaL <raylanna.l@hotmail.com>
2023-12-11 20:16:26 -0300

    feat: :sparkles: novas funções

--- main.c
+++ main.c
@@ -34,14 +34,18 @@ void criarTabela(FILE *arquivo);
 void removerQuebraLinha(char *string);
 void limparBuffer();
 void listarTabelas();
+int pegarDados(char string[], Coluna *coluna, int opcao);
+void criarLinha(FILE *arquivo, char nomeTabela[]);
 
 
 int main() 
 {
     FILE *arquivoTabela = NULL;
+    char nomeTabela[MAX_TAM_NOME];
     int escolhaMenu = 0;
     scanf("%d", &escolhaMenu);
-    
+    limparBuffer();
+
     switch (escolhaMenu)
     {
     case 1: 
@@ -51,6 +55,22 @@ int main()
         listarTabelas();
         break;
     case 3: //criar nova linha na tabela
+        printf("Informe em qual tabela (nome e extensão) deseja adicionar uma nova linha:  \n");
+        fgets(nomeTabela, MAX_TAM_NOME, stdin);
+        removerQuebraLinha(nomeTabela);
+
+        arquivoTabela = fopen(nomeTabela, "r");
+        if (arquivoTabela == NULL)
+        {
+            printf("Nome de tabela invalido ou inexistente.\n");
+            printf("As tabelas existentes são: \n");
+            listarTabelas();
+            return 1;
+        }
+        else {
+            fclose(arquivoTabela);
+        }
+        criarLinha(arquivoTabela, nomeTabela);
         break;
     case 4: //criar nova coluna na tabela
         break;
@@ -73,7 +93,6 @@ int main()
 void criarTabela(FILE *arquivo) {
     char nomeTabela[MAX_TAM_NOME];
 
-    limparBuffer();
     printf("Informe o nome da tabela: ");
     fgets(nomeTabela, MAX_TAM_NOME, stdin);
     removerQuebraLinha(nomeTabela);
@@ -89,14 +108,14 @@ void criarTabela(FILE *arquivo) {
         // Arquivo com o mesmo nome já existe
         printf("Um arquivo com o nome %s já existe.\n", nomeTabela);
         fclose(arquivo);
-        return 1;
+        return;
     }
     // Cria o arquivo em modo escrita
     arquivo = fopen(nomeTabela, "w");
     if (arquivo == NULL)
     {
         printf("Erro ao criar o arquivo.\n");
-        return 1;
+        return;
     }
     
     int quantColunas = 0;
@@ -111,6 +130,7 @@ void criarTabela(FILE *arquivo) {
 
     Coluna coluna[quantColunas];
     char nomeColunaImput[MAX_TAM_NOME];
+
     // Criação da primeira linha da tabela que contém os nomes
     for (int i = 0; i < quantColunas; i++) 
     {
@@ -120,7 +140,6 @@ void criarTabela(FILE *arquivo) {
             fgets(nomeColunaImput, MAX_TAM_NOME, stdin);// Leitura do nome da coluna
             removerQuebraLinha(nomeColunaImput);
             strcpy(coluna[i].nomeColuna, nomeColunaImput);
-            fprintf(arquivo,"%s",coluna[i].nomeColuna);// Escreve no arquivo o nome da coluna
         }
         else
         {
@@ -129,14 +148,10 @@ void criarTabela(FILE *arquivo) {
             removerQuebraLinha(nomeColunaImput);
             coluna[i].tipoColuna = verificaTipo(nomeColunaImput, coluna, i);// Separa o nome da coluna do seu tipo de dado
             strcpy(coluna[i].nomeColuna, nomeColunaImput);
-            fprintf(arquivo,"||%s", coluna[i].nomeColuna);// Escreve no arquivo o nome da coluna
         }
-        
     }
-    fprintf(arquivo,";;\n");// Termina a linha com ";;" e muda o ponteiro para iniciar a proxima linha
-    fseek(arquivo, 0, SEEK_SET); // Retorna o ponteiro para inicio do arquivo para escrever os tipos de dados recebidos
 
-    for (int i = 1; i < quantColunas; i++) // Escreve os tipos de dados na primeira linha
+    for (int i = 0; i < quantColunas; i++) // Escreve os tipos de dados na primeira linha
     {
         if (i == 0)
         {
@@ -147,54 +162,23 @@ void criarTabela(FILE *arquivo) {
             fprintf(arquivo, "||%d", coluna[i].tipoColuna);
         }   
     }
-    fprintf(arquivo,"\n");
-    
-    //primeira linha com valores
-    unsigned int indexChavePrimaria = 0;
+    fputc('\n', arquivo);
+
     for (int i = 0; i < quantColunas; i++)
     {
         if (i == 0)
         {
-            indexChavePrimaria++;
-            fprintf(arquivo,"%d",indexChavePrimaria);// Escreve o valor da coluna chave primaria
+            fprintf(arquivo,"%s",coluna[i].nomeColuna);// Escreve no arquivo o nome da coluna
         }
-        else
-        {
-            printf("Digite o valor da coluna %s: ", coluna[i].nomeColuna);
-            char valorInput[MAX_TAM_VALOR];
-            fgets(valorInput, MAX_TAM_VALOR, stdin);
-            removerQuebraLinha(valorInput);
-
-            switch (coluna[i].tipoColuna) // Verifica o tipo de dado e escreve no arquivo depois da conversão necessária
-            {
-            case TIPO_INT:
-                coluna[i].valorColuna.valorInt = atoi(valorInput);
-                fprintf(arquivo,"||%d", coluna[i].valorColuna.valorInt);
-                break;
-            case TIPO_CHAR:
-                coluna[i].valorColuna.valorChar = valorInput[0];
-                fprintf(arquivo,"||%c", coluna[i].valorColuna.valorChar);
-                break;
-            case TIPO_FLOAT:
-                coluna[i].valorColuna.valorFloat = atof(valorInput);
-                fprintf(arquivo,"||%f", coluna[i].valorColuna.valorFloat);
-                break;
-            case TIPO_DOUBLE:
-                coluna[i].valorColuna.valorDouble = atof(valorInput);
-                fprintf(arquivo,"||%lf", coluna[i].valorColuna.valorDouble);
-                break;
-            case TIPO_STRING:
-                strcpy(coluna[i].valorColuna.valorString, valorInput);
-                fprintf(arquivo,"||%s", coluna[i].valorColuna.valorString);
-                break;
-            default:
-                break;
-            }
+        else {
+            fprintf(arquivo,"||%s", coluna[i].nomeColuna);// Escreve no arquivo o nome da coluna
         }
+        
     }
-    fprintf(arquivo,"\n");// Termina a linha com ";;" e muda o ponteiro para iniciar a proxima linha
-
+    fprintf(arquivo,"\n");
     fclose(arquivo);// Fecha o arquivo
+
+    criarLinha(arquivo, nomeTabela);
 
 }
 
@@ -253,7 +237,7 @@ void listarTabelas() {
     {
         while ((ent = readdir(dir)) != NULL)
         {
-            // Verifica se o nome do arquivo termina com a extensão "".itp"
+            // Verifica se o nome do arquivo termina com a extensão ".itp"
             int tamanhoNome = strlen(ent->d_name);
             int tamanhoExtensao = strlen(extensao);
             if (tamanhoNome >= tamanhoExtensao && strcmp(ent->d_name + tamanhoNome - tamanhoExtensao, extensao) == 0)
@@ -268,19 +252,212 @@ void listarTabelas() {
     }
 }
 
-void criarLinha(FILE *arquivo, char nomeTabela[MAX_TAM_NOME]) {
-    char linhaDeTipos[MAX_TAM_NOME]; // Considerando que terão no máximo 15 colunas
-    char linhaNomesColunas[255]; 
+int pegarDados(char string[], Coluna *coluna, int opcao) {
+    char *pedacoLinha;
+    int contColunas = 0, i = 0;
+
+    pedacoLinha = strtok(string, "||");
+    switch (opcao)
+    {
+    case 1:
+        while (pedacoLinha != NULL)
+        {
+            int tipo = atoi(pedacoLinha);
+            pedacoLinha = strtok(NULL, "||");
+            coluna[i].tipoColuna = tipo;
+            contColunas++;
+            i++;
+        }
+        return contColunas;
+        break;
+    case 2:
+        while (pedacoLinha != NULL)
+        {
+            strcpy(coluna[i].nomeColuna, pedacoLinha);
+            pedacoLinha = strtok(NULL, "||");
+            i++;
+        }
+        break;
+    case 3:
+        while (pedacoLinha != NULL)
+        {
+            switch (coluna[i].tipoColuna)
+            {
+            case 0:
+                coluna[i].valorColuna.valorChaveP = atoi(pedacoLinha);
+                break;
+            case 1:
+                coluna[i].valorColuna.valorChar = pedacoLinha[0];
+                break;
+            case 2:
+                coluna[i].valorColuna.valorFloat = atof(pedacoLinha);
+                break;
+            case 3:
+                coluna[i].valorColuna.valorDouble = atof(pedacoLinha);
+                break;
+            case 4:
+                strcpy(coluna[i].valorColuna.valorString, pedacoLinha);
+                break;
+            default:
+                printf("Tipo de dado inválidohfhfh.\n");
+                return -1;
+                break;
+            }
+            pedacoLinha = strtok(NULL, "||");
+            i++;
+        }
+        break;
+    
+    default:
+        break;
+    }
+    return 0; 
+}
+
+
+void criarLinha(FILE *arquivo, char nomeTabela[]) {
+    int quantColunas = 5; // Considerando que a tabela contem inicialmente no máximo 5 colunas
+    Coluna linhaNova[quantColunas];
+    char linhaDeTipos[MAX_TAM_NOME];
+    char linhaAtual[255]; 
+
     arquivo = fopen(nomeTabela, "r+");
     if (arquivo == NULL)
     {
         printf("Erro ao abrir o arquivo");
-        return 1;
+        return;
     }
+
     fscanf(arquivo, "%s", linhaDeTipos); // Lê a primeira linha do arquivo que informa os tipos do dados
-    fscanf(arquivo, "%s", linhaNomesColunas); // Lê a segunda linha do arquivo que informa nos nomes das colunas
+    quantColunas = pegarDados(linhaDeTipos, linhaNova, 1);
     
+    fscanf(arquivo, "%s", linhaAtual); // Lê a segunda linha do arquivo que informa nos nomes das colunas
+    pegarDados(linhaAtual, linhaNova, 2);
+
+    int contadorLinhasRegistro = 0;
+    int quantLinhas = 5;
+    int listaChavesP[quantLinhas];
+
+    while (fgets(linhaAtual, 255, arquivo) != NULL)
+    {
+        pegarDados(linhaAtual, linhaNova, 3);
+        listaChavesP[contadorLinhasRegistro] = linhaNova[0].valorColuna.valorChaveP;
+        contadorLinhasRegistro++;
+    }
     
+
+    if (contadorLinhasRegistro == 0) // Escreve o primeiro registro
+    {
+        for (int i = 0; i < quantColunas; i++)
+        {
+            if (i == 0)
+            {
+                fprintf(arquivo,"1");// Inicia a chave primaria do primeiro registro = 1
+            }
+            else
+            {
+                printf("Digite o valor da coluna %s: ", linhaNova[i].nomeColuna);
+                char valorInput[MAX_TAM_VALOR];
+                fgets(valorInput, MAX_TAM_VALOR, stdin);
+                removerQuebraLinha(valorInput);
+
+                switch (linhaNova[i].tipoColuna) // Verifica o tipo de dado e escreve no arquivo depois da conversão necessária
+                {
+                case TIPO_INT:
+                    linhaNova[i].valorColuna.valorInt = atoi(valorInput);
+                    fprintf(arquivo,"||%d", linhaNova[i].valorColuna.valorInt);
+                    break;
+                case TIPO_CHAR:
+                    linhaNova[i].valorColuna.valorChar = valorInput[0];
+                    fprintf(arquivo,"||%c", linhaNova[i].valorColuna.valorChar);
+                    break;
+                case TIPO_FLOAT:
+                    linhaNova[i].valorColuna.valorFloat = atof(valorInput);
+                    fprintf(arquivo,"||%f", linhaNova[i].valorColuna.valorFloat);
+                    break;
+                case TIPO_DOUBLE:
+                    linhaNova[i].valorColuna.valorDouble = atof(valorInput);
+                    fprintf(arquivo,"||%lf", linhaNova[i].valorColuna.valorDouble);
+                    break;
+                case TIPO_STRING:
+                    strcpy(linhaNova[i].valorColuna.valorString, valorInput);
+                    fprintf(arquivo,"||%s", linhaNova[i].valorColuna.valorString);
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+        fputc('\n', arquivo);
+    }
+    else
+    {
+        unsigned int indexNovaLinha = 1;
+        int jaTemEsse = 0;
+        for (int i = indexNovaLinha; i <= contadorLinhasRegistro; i++)
+        {
+            jaTemEsse = 0;
+            for (int j = 0; j < contadorLinhasRegistro; j++)
+            {
+                if (listaChavesP[j] == i)
+                {
+                    jaTemEsse = 1;
+                    break;
+                }
+            }
+            if (jaTemEsse == 0)
+            {
+                indexNovaLinha = i;
+                break;
+            }
+        }
+
+        fseek(arquivo, 0, SEEK_END); // Posiciona o ponteiro no final do arquivo para inserir a nova linha
+
+        for (int i = 0; i < quantColunas; i++)
+        {
+            if (i == 0)
+            {
+                fprintf(arquivo,"%d", indexNovaLinha);
+            }
+            else
+            {
+                printf("Digite o valor da coluna %s: ", linhaNova[i].nomeColuna);
+                char valorInput[MAX_TAM_VALOR];
+                fgets(valorInput, MAX_TAM_VALOR, stdin);
+                removerQuebraLinha(valorInput);
+
+                switch (linhaNova[i].tipoColuna) // Verifica o tipo de dado e escreve no arquivo depois da conversão necessária
+                {
+                case TIPO_INT:
+                    linhaNova[i].valorColuna.valorInt = atoi(valorInput);
+                    fprintf(arquivo,"||%d", linhaNova[i].valorColuna.valorInt);
+                    break;
+                case TIPO_CHAR:
+                    linhaNova[i].valorColuna.valorChar = valorInput[0];
+                    fprintf(arquivo,"||%c", linhaNova[i].valorColuna.valorChar);
+                    break;
+                case TIPO_FLOAT:
+                    linhaNova[i].valorColuna.valorFloat = atof(valorInput);
+                    fprintf(arquivo,"||%f", linhaNova[i].valorColuna.valorFloat);
+                    break;
+                case TIPO_DOUBLE:
+                    linhaNova[i].valorColuna.valorDouble = atof(valorInput);
+                    fprintf(arquivo,"||%lf", linhaNova[i].valorColuna.valorDouble);
+                    break;
+                case TIPO_STRING:
+                    strcpy(linhaNova[i].valorColuna.valorString, valorInput);
+                    fprintf(arquivo,"||%s", linhaNova[i].valorColuna.valorString);
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+        fputc('\n', arquivo);
+    }
+    fclose(arquivo);
 }
+
 
   
